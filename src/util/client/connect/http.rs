2601a29ccadea92284bf057c3afc52e9240b1941
Arshia Ghafoori <arshia001@live.com>
2025-08-22 09:19:03 +0000

    Add WASIX patches and build settings

--- src/util/client/connect/http.rs
+++ src/util/client/connect/http.rs
@@ -876,7 +876,7 @@ fn connect(
     )
     .map_err(ConnectError::m("tcp bind local error"))?;
 
-    #[cfg(unix)]
+    #[cfg(any(unix, target_vendor = "wasmer"))]
     let socket = unsafe {
         // Safety: `from_raw_fd` is only safe to call if ownership of the raw
         // file descriptor is transferred. Since we call `into_raw_fd` on the
